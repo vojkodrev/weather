5360f1849d2048d0b6f7a044784b48e6938900ca
Vojko Drev <vojkodrev@gmail.com>
2018-02-14 21:34:36 +0100

    Scss navigation bar.

--- src/app/components/app/app.component.ts
+++ src/app/components/app/app.component.ts
@@ -5,7 +5,7 @@ import { NavigationVisibilityService } from '../../services/navigation-visibilit
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'app';
