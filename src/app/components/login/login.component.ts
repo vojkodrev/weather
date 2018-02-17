ec83be1f166d457d862ae57baf1a69ca4a7651ac
Vojko Drev <vojkodrev@gmail.com>
2018-02-17 09:01:38 +0100

    Login scss. Simplified login html and css.

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -5,7 +5,7 @@ import { NavigationVisibilityService } from '../../services/navigation-visibilit
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
